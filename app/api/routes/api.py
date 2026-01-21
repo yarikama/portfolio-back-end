d0adb9980e71a873498d08e2c74a6d9a758f3caa
yarikama <amaki30226@gmail.com>
2026-01-21 00:20:26 -0600

    feature: add auth in api router

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,6 +1,7 @@
-from api.routes import predictor, projects
+from api.routes import auth, predictor, projects
 from fastapi import APIRouter
 
 router = APIRouter()
+router.include_router(auth.router, tags=["auth"], prefix="/v1")
 router.include_router(predictor.router, tags=["predictor"], prefix="/v1")
 router.include_router(projects.router, tags=["projects"], prefix="/v1")
